c0ff073be34488557e8cd43b83577ff3703ed791
Sukka <isukkaw@gmail.com>
2022-07-12 14:55:12 +0800

    Chain benchmark

--- benchmark/bench.ts
+++ benchmark/bench.ts
@@ -1,10 +1,8 @@
 import { benchSlugize } from './slugize'
 import { benchStripTags } from './strip_html'
 
-benchStripTags().catch((e) => {
-  console.error(e)
-})
-
-benchSlugize().catch((e) => {
-  console.error(e)
-})
+benchStripTags()
+  .then(() => benchSlugize())
+  .catch((e) => {
+    console.error(e)
+  })
