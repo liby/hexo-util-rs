d639a1a57050cd470bd75041f60f84183f5c5b43
Bryan Lee <doslby@gmail.com>
2022-07-19 17:45:05 +0800

    chore: add benchmarks and format code

--- benchmark/bench.ts
+++ benchmark/bench.ts
@@ -1,8 +1,10 @@
+import { benchIsExternalLink } from './is_external_link'
 import { benchSlugize } from './slugize'
 import { benchStripTags } from './strip_html'
 
 benchStripTags()
   .then(() => benchSlugize())
+  .then(() => benchIsExternalLink())
   .catch((e) => {
     console.error(e)
   })
---
+++ benchmark/is_external_link.ts
@@ -0,0 +1,48 @@
+import b from 'benny'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { isExternalLink as hexoIsExternalLink } from 'hexo-util'
+
+import { isExternalLink } from '../index'
+
+export async function benchIsExternalLink() {
+  // eslint-disable-next-line no-console
+  await b.suite(
+    'Internal absolute url',
+    b.add('hexo-util-rs', () => {
+      isExternalLink('https://hexo.io/docs/', 'https://hexo.io/')
+    }),
+    b.add('hexo-util', () => {
+      hexoIsExternalLink('https://hexo.io/docs/', 'https://hexo.io/')
+    }),
+
+    b.cycle(),
+    b.complete(),
+  )
+
+  await b.suite(
+    'External absolute url',
+    b.add('hexo-util-rs', () => {
+      isExternalLink('https://example.com/example/', 'https://hexo.io/')
+    }),
+    b.add('hexo-util', () => {
+      hexoIsExternalLink('https://example.com/example/', 'https://hexo.io/')
+    }),
+
+    b.cycle(),
+    b.complete(),
+  )
+
+  await b.suite(
+    'Relative absolute url',
+    b.add('hexo-util-rs', () => {
+      isExternalLink('/example/', 'https://hexo.io/')
+    }),
+    b.add('hexo-util', () => {
+      hexoIsExternalLink('/example/', 'https://hexo.io/')
+    }),
+
+    b.cycle(),
+    b.complete(),
+  )
+}
