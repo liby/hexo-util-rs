fe32b34d9ca01fdd51a4d2e4b68df31f59d11923
bdbai <bdbaiapp@163.com>
2022-07-20 20:03:12 +0800

    doc: comment on isExternalLink js wrapper

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const Cache = require('./common/cache')
 const { isExternalLink: isExternalLinkRs, slugize, stripTags } = require('./utils')
 
 const externalLinkCache = new Cache()
+
 module.exports.isExternalLink = function isExternalLink(input, sitehost, exclude) {
   return externalLinkCache.apply(`${input}-${sitehost}-${exclude}`, () => {
-    // Return false early for internal link
+    // Fast path: return false early for internal link
+    // This does not involve costly operations like URL parsing or FFI interop.
     if (!/^(\/\/|http(s)?:)/.test(input)) return false
+
+    // Slow path: call Rust implementation for better URL parsing performance.
     return isExternalLinkRs(input, sitehost, exclude)
   })
 }
